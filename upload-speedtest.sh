18cb98534b83d018cc25c5e87146d4a2be624eda
dpfrakes@dpfrakes-Dimension-2400 <dpfrakes@gmail.com>
2016-07-07 23:39:12 -0400

    latest auto-upload script added to crontab

--- upload-speedtest.sh
+++ upload-speedtest.sh
@@ -1,4 +1,8 @@
-git add ~/.local/speedtest-cli/speedlog.csv
-git commit -m "[AUTO UPLOAD SPEEDLOG]"
-git push
+# PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+
+cd ~/speedtest-cli
+
+/usr/bin/git add speedlog.csv
+/usr/bin/git commit -m "[AUTO UPLOAD SPEEDLOG]"
+/usr/bin/git push
 
